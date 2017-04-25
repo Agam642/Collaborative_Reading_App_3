ff252e27ae77f98e4cb2111bad2867b9d433f794
Student <Student@zpr-l0007074.local>
2017-04-25 08:45:27 -0400

    Reuploaded files

---
+++ Collaborative_Reading_App_3/FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  Collaborative_Reading_App_3
+//
+//  Created by Student on 2017-04-25.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: BaseCell {
+
+    //override func setupViews() {
+        
+    //}
+}
