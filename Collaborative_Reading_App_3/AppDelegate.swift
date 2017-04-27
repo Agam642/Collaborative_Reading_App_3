33ece1de2eb4814961d73b64ee54c81b61531043
Saif Ali <saif@Saifs-MacBook-Air.local>
2017-04-27 09:25:33 -0400

    Added menu navigation

--- Collaborative_Reading_App_3/AppDelegate.swift
+++ Collaborative_Reading_App_3/AppDelegate.swift
@@ -17,27 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        
-        let layout = UICollectionViewFlowLayout()
-        window?.rootViewController = UINavigationController(rootViewController: HomeController(collectionViewLayout: layout))
-        
-        UINavigationBar.appearance().barTintColor = UIColor.rgb(69, green: 179, blue: 231)
-        
-        // get rid of black bar underneath navbar
-        UINavigationBar.appearance().shadowImage = UIImage()
-        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
-        
-        application.statusBarStyle = .lightContent
-        
-        let statusBarBackgroundView = UIView()
-        statusBarBackgroundView.backgroundColor = UIColor.rgb(26, green: 143, blue: 198)
-        
-        window?.addSubview(statusBarBackgroundView)
-        window?.addConstraintsWithFormat("H:|[v0]|", views: statusBarBackgroundView)
-        window?.addConstraintsWithFormat("V:|[v0(20)]", views: statusBarBackgroundView)
-        
         return true
     }
     
