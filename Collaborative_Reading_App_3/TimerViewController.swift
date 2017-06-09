375435571f528f10fb48eb0b43ebd53c83e3eed9
Student <Student@zpr-l0007074.local>
2017-06-09 09:00:10 -0400

    TimerviewController added

--- Collaborative_Reading_App_3/TimerViewController.swift
+++ Collaborative_Reading_App_3/TimerViewController.swift
@@ -1,35 +1,135 @@
 //
-//  TimerViewController.swift
+//  ViewController.swift
 //  Collaborative_Reading_App_3
 //
-//  Created by Student on 2017-06-09.
-//  Copyright © 2017 Student. All rights reserved.
+//  Created by Saif Al-Din Ali on 2017-04-11.
+//  Copyright © 2017 Saif Al-Din Ali. All rights reserved.
 //
 
 import UIKit
 
-class TimerViewController: UIViewController {
-
+class TimerViewController: UIViewController, CountdownTimerDelegate {
+    
+    @IBOutlet weak var progressBar: ProgressBar!
+    @IBOutlet weak var hours: UILabel!
+    @IBOutlet weak var minutes: UILabel!
+    @IBOutlet weak var seconds: UILabel!
+    @IBOutlet weak var counterView: UIStackView!
+    @IBOutlet weak var stopBtn: UIButton!
+    @IBOutlet weak var startBtn: UIButton!
+    
+    let countdownTimer = CountdownTimer()
+    var countdownTimerDidStart = false
+    
+    // Variables usd for the timer
+    var selectedSecs : Int = 0
+    var selectedMins : Int = 30
+    var selectedHours : Int = 0
+    
+    
+    //let selectedHours : Int = selectedMins % 60
+    
+    lazy var messageLabel: UILabel = {
+        let label = UILabel(frame:CGRect.zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 24.0, weight: UIFontWeightLight)
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        label.text = "Done!"
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        countdownTimer.setTimer(hours: selectedHours, minutes: selectedMins, seconds: selectedSecs)
+        countdownTimer.delegate = self
+        progressBar.setProgressBar(hours: selectedHours, minutes: selectedMins, seconds: selectedSecs)
+        stopBtn.isEnabled = false
+        stopBtn.alpha = 0.5
+        seconds.text = String(selectedSecs)
+        minutes.text = String(selectedMins)
+        hours.text = String(selectedHours)
+        
+        view.addSubview(messageLabel)
+        
+        var constraintCenter = NSLayoutConstraint(item: messageLabel, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0)
+        self.view.addConstraint(constraintCenter)
+        constraintCenter = NSLayoutConstraint(item: messageLabel, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0)
+        self.view.addConstraint(constraintCenter)
+        
+        messageLabel.isHidden = true
+        counterView.isHidden = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .lightContent
     }
-    */
-
+    
+    
+    //MARK: - Delegates
+    
+    func countdownTime(time: (hours: String, minutes: String, seconds: String)) {
+        hours.text = time.hours
+        minutes.text = time.minutes
+        seconds.text = time.seconds
+    }
+    
+    
+    func countdownTimerDone() {
+        
+        counterView.isHidden = true
+        messageLabel.isHidden = false
+        seconds.text = String(selectedSecs)
+        countdownTimerDidStart = false
+        stopBtn.isEnabled = false
+        stopBtn.alpha = 0.5
+        startBtn.setTitle("START",for: .normal)
+        
+        print("countdownTimerDone")
+    }
+    
+    
+    
+    
+    //MARK: - Actions
+    
+    @IBAction func startTimer(_ sender: UIButton) {
+        
+        messageLabel.isHidden = true
+        counterView.isHidden = false
+        
+        stopBtn.isEnabled = true
+        stopBtn.alpha = 1.0
+        
+        if !countdownTimerDidStart{
+            countdownTimer.start()
+            progressBar.start()
+            countdownTimerDidStart = true
+            startBtn.setTitle("PAUSE",for: .normal)
+            
+        }else{
+            countdownTimer.pause()
+            progressBar.pause()
+            countdownTimerDidStart = false
+            startBtn.setTitle("RESUME",for: .normal)
+        }
+    }
+    
+    
+    @IBAction func stopTimer(_ sender: UIButton) {
+        countdownTimer.stop()
+        progressBar.stop()
+        countdownTimerDidStart = false
+        stopBtn.isEnabled = false
+        stopBtn.alpha = 0.5
+        startBtn.setTitle("START",for: .normal)
+    }
+    
+    
+    
+    
+    
 }
+
