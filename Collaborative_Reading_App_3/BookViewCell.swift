4a6a9dc99f648751c3cdaf101a6f756816947878
Student <Student@zpr-l0007074.local>
2017-06-09 09:19:04 -0400

    BookViewCell added

---
+++ Collaborative_Reading_App_3/BookViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BookViewCell.swift
+//  Collaborative_Reading_App_3
+//
+//  Created by Saif Al-Din on 2017-05-25.
+//  Copyright © 2017 Saif Al-Din. All rights reserved.
+//
+
+import UIKit
+
+class BookViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imgCell: UIImageView!
+    @IBOutlet var lblCell: UILabel!
+    
+}
